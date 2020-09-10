bc4c9dff0bc7bf1c98486e103aa92493c207dd22
Patrick <patrickmceldowney@outlook.com>
2020-09-10 11:35:45 -0500

    removing welcome page

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
-import Welcome from './containers/Welcome'
-
 export default function Routes() {
   return (
     <Switch>
       <Route exact path='/'>
         <Home />
-      </Route>
-      <Route exact path='/welcome'>
-        <Welcome />
       </Route>
       <Route>
         <NotFound />
